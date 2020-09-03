a9d25cd8947e5319bc7cb936d58a96192f6e62de
Ashwin Gonsalves <ashwingonsalves@gmail.com>
2020-09-03 11:30:40 +0530

    Initial commit

---
+++ tests/twoTest.php
@@ -0,0 +1,52 @@
+<?php
+require 'vendor/autoload.php';
+
+    class twoTest extends PHPUnit\Extensions\Selenium2TestCase
+    {
+
+        public static $browsers = array(
+          array(
+            'host'=> 'hub.browserstack.com',
+            'port' => 80,
+            "browserName" => "chrome",
+            'desiredCapabilities' => array(
+                "os" => "Windows",
+                "os_version" => "10",
+                "browser_version" => "latest",
+                "browserstack.user" => "BROWSERSTACK_USERNAME",
+                "browserstack.key" => "BROWSERSTACK_ACCESS_KEY",
+                "project" => "PHPUnit Paratest Samples ",
+                "build" => "PHPUnit Paratest v1"
+            )
+          ),
+          array(
+            'host'=> 'hub.browserstack.com',
+            'port' => 80,
+            "browserName" => "firefox",
+            'desiredCapabilities' => array(
+                "os" => "Windows",
+                "os_version" => "10",
+                "browser_version" => "latest",
+                "browserstack.user" => "BROWSERSTACK_USERNAME",
+                "browserstack.key" => "BROWSERSTACK_ACCESS_KEY",
+                "project" => "PHPUnit Paratest Samples ",
+                "build" => "PHPUnit Paratest v1"
+            )
+          )
+        );
+
+        protected function setUp(): void
+        {
+            // $this->setBrowser('chrome');
+            $this->setBrowserUrl('http://www.example.com/');
+        }
+
+        public function testTitle()
+        {
+            $this->url('http://www.example.com/');
+            // sleep(10);
+            $this->assertEquals('Example Domain', $this->title());
+        }
+
+    }
+?>
